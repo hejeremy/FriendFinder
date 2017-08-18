564c3b29480d901dbe79e34f4bd66656278680bb
jeremyhe <jeremyhe1@gmail.com>
2017-08-18 18:33:34 -0400

    stuff

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,9 +5,53 @@ module.exports = function(app){
         res.json(friends);
     });
 
-    app.post("/api/tables", function(req, res) {
-        friends.push(req.body);
-        console.log('apiRoutes.js app.post happened.');
-        res.json(true);
+    app.post("/api/friends", function(req, res) {
+        if (checkExist(req.body)) {
+            console.log('Finding friend...');
+            res.json(findBestFit(req.body));
+            friends.push(req.body);
+        } else {
+            console.log('You are already on the friends list!');
+            res.json(false);
+        }
     });
+}
+
+function checkExist(inputObj) {
+    let newFriend = true;
+    for (let i=0; i<friends.length; i++) {
+        if (friends[i].name === inputObj.name && friends[i].photo === inputObj.photo) {
+            newFriend = false;
+        }
+    }
+    return newFriend;
+}
+
+function findBestFit(inputObj) {
+    var friend = {
+        name: 'No Results',
+        photo: 'https://cdn.dribbble.com/users/308895/screenshots/2598725/no-results.gif',
+        score: 5
+    }
+    for (let i=0; i<friends.length; i++) {
+        let score = testCompatibility(inputObj, friends[i]);
+        if (score < friend.score) {
+            friend = {
+                name: friends[i].name,
+                photo: friends[i].photo,
+                score: score
+            }
+        }
+    }
+    return friend;
+}
+
+function testCompatibility(obj1, obj2) {
+    let totalScore = 0;
+    for (let i=0; i<obj1.scores.length; i++) {
+        totalScore += Math.abs(obj1.scores[i] - obj2.scores[i]);
+    }
+
+    let averageScore = totalScore / obj1.scores.length;
+    return averageScore;
 }
